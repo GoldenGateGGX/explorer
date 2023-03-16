59192e6ae3e43e327e22b69bcbbb3bdf2acfe721
TarasPotsikaylo <potsikaylot@gmail.com>
2023-03-16 15:43:08 +0200

    added EVM to Substrate address converter (#12)
    * added EVM to Substrate address converter
    
    * added EVM-Substrate notification (#14)
    
    * added EVM-Substrate notification
    
    * changed nitification logic
    
    * fixed linter
    
    * deleted redundant code
    
    * fixed linter
    
    * mminor changes
    
    * implemented logic of getting SS58 prefix from api

--- packages/apps/src/Menu/index.tsx
+++ packages/apps/src/Menu/index.tsx
@@ -176,6 +176,12 @@ const StyledDiv = styled.div`
     width: 100%;
     max-width: var(--width-full);
     margin: 0 auto;
+
+    .smallHide {
+      + div {
+        color: #fff;
+      }
+    }
   }
 
   &.isLoading {
